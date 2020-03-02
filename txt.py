18b17939e32d1275b2531f8588586fd2bf511e7e
Amireshhhh <46416763+Amireshhhh@users.noreply.github.com>
2020-03-02 02:58:52 -0600

    Update txt.py

--- txt.py
+++ txt.py
@@ -18,3 +18,12 @@ def loadData(X_train, X_test,MAX_NB_WORDS=75000):
     X_test = vectorizer_x.transform(X_test).toarray()
     print("tf-idf with",str(np.array(X_train).shape[1]),"features")
     return (X_train,X_test)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+def TFIDF(X_train, X_test, MAX_NB_WORDS=75000):
+    vectorizer_x = TfidfVectorizer(max_features=MAX_NB_WORDS)
+    X_train = vectorizer_x.fit_transform(X_train).toarray()
+    X_test = vectorizer_x.transform(X_test).toarray()
+    print("tf-idf with", str(np.array(X_train).shape[1]), "features")
+    return (X_train, X_test)
