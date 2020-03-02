1919bb37e196bba2e143a8d4fc29063ce3fe4b65
Amireshhhh <46416763+Amireshhhh@users.noreply.github.com>
2020-03-02 02:56:15 -0600

    Update txt.py

--- txt.py
+++ txt.py
@@ -9,3 +9,12 @@ for i in range(df_train["text"].size):
     result=df_train["text"][i].translate(str.maketrans('','', string.punctuation))
     df_train["text"][i]=result
     df_train["text"][i]=df_train["text"][i].strip()
+
+  ##TF-ID transform  
+from sklearn.feature_extraction.text import TfidfTransformer
+def loadData(X_train, X_test,MAX_NB_WORDS=75000):
+    vectorizer_x = TfidfVectorizer(max_features=MAX_NB_WORDS)
+    X_train = vectorizer_x.fit_transform(X_train).toarray()
+    X_test = vectorizer_x.transform(X_test).toarray()
+    print("tf-idf with",str(np.array(X_train).shape[1]),"features")
+    return (X_train,X_test)
